2cb5536590cbe7e65d8ae8a401b64242d9152805
Alley Gautam <112246176+AlleyGautam08@users.noreply.github.com>
2023-05-21 18:01:14 +0530

    Add files via upload

---
+++ word-replacement.py
@@ -0,0 +1,8 @@
+def replace_word():
+
+     str = "Hi guys, i am alley, and hi hi hi"
+     word_to_replace = input("Enter the word to replace: ")
+     word_replacement = input("Enter the word  replacement: ")
+     print(str.replace(word_to_replace, word_replacement))
+
+replace_word()
